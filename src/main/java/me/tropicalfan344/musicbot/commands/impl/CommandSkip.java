4828e07546a5f9dcb71356eccdbf739a0bd8ecd9
TropicalFan344 <1080101@gm.fdhs.tyc.edu.tw>
2022-06-18 19:39:17 +0800

    Skip with index

--- src/main/java/me/tropicalfan344/musicbot/commands/impl/CommandSkip.java
+++ src/main/java/me/tropicalfan344/musicbot/commands/impl/CommandSkip.java
@@ -6,10 +6,12 @@ import me.tropicalfan344.musicbot.commands.MusicCommand;
 import me.tropicalfan344.musicbot.engines.Track;
 import me.tropicalfan344.musicbot.utils.SimpleEmbedGenerator;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 public class CommandSkip extends MusicCommand {
     public CommandSkip() {
-        super("skip", "Skip the music that's playing");
+        super("skip", "Skip the music that's playing", new OptionData(OptionType.INTEGER, "index", "index of the song you want skip to"));
     }
 
     @Override
@@ -17,6 +19,15 @@ public class CommandSkip extends MusicCommand {
         GuildMusicManager musicManager = GuildMusicManager.getMusicManager(musicBot, event.getGuild());
         if (musicManager.getQueue().isEmpty()) {
             throw new CommandException("Couldn't skip anything");
+        }
+        if (event.getOption("index") != null) {
+            if (event.getOption("index").getAsInt() > musicManager.getQueue().size() + 1) {
+                throw  new CommandException("There is not that much songs in queue");
+            }
+            for (int i = 1; i < event.getOption("index").getAsInt(); i++) {
+                musicManager.skip();
+            }
+            event.getInteraction().replyEmbeds(SimpleEmbedGenerator.generateSuccessfulEmbed("Skiped " + (event.getOption("index").getAsInt() - 1) + "song(s) in queue"));
         }
         Track skippedTrack = musicManager.getQueue().get(0);
         musicManager.skip();
