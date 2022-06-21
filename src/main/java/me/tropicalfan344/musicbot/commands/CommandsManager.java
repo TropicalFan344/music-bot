50ebf058c5c7d1c85f9a893d93e8a519da1fc546
TropicalFan344 <1080101@gm.fdhs.tyc.edu.tw>
2022-06-22 02:25:21 +0800

    new server

--- src/main/java/me/tropicalfan344/musicbot/commands/CommandsManager.java
+++ src/main/java/me/tropicalfan344/musicbot/commands/CommandsManager.java
@@ -54,7 +54,8 @@ public class CommandsManager {
 
         String[] guilds = new String[] {
                 "979552134850834492",
-                "854997016996216862"
+                "854997016996216862",
+                "988871600701075557"
         };
         for (String guild : guilds) {
             if (musicBot.getJda().getGuildById(guild) == null) {
