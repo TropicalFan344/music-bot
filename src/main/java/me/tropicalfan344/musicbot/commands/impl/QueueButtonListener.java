f6ed3010db1359de96a370e1cfd1174c2aa7e0c5
TropicalFan344 <1080101@gm.fdhs.tyc.edu.tw>
2022-07-07 23:32:37 +0800

    um...

--- src/main/java/me/tropicalfan344/musicbot/commands/impl/QueueButtonListener.java
+++ src/main/java/me/tropicalfan344/musicbot/commands/impl/QueueButtonListener.java
@@ -36,7 +36,7 @@ public class QueueButtonListener extends ListenerAdapter {
                 builder.setTitle("Songs in Queue");
                 String out = "";
                 int requestedPage = page;
-                int pageAmount = 10;
+                int pageAmount = 15;
                 int totalPages = (int) Math.ceil(manager.getQueue().size() * 1.0 / pageAmount);
                 requestedPage = Math.min(Math.max(requestedPage, 1), totalPages);
                 int startIndex = (requestedPage - 1) * pageAmount; // Inclusive
