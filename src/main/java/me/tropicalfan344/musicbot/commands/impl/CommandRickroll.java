c64f21e078f7ec791bd162e36c56d7abdf4fe9b1
TropicalFan344 <1080101@gm.fdhs.tyc.edu.tw>
2022-08-06 11:41:44 +0800

    check

--- src/main/java/me/tropicalfan344/musicbot/commands/impl/CommandRickroll.java
+++ src/main/java/me/tropicalfan344/musicbot/commands/impl/CommandRickroll.java
@@ -1,10 +1,13 @@
 package me.tropicalfan344.musicbot.commands.impl;
 
 import me.tropicalfan344.musicbot.GuildMusicManager;
+import me.tropicalfan344.musicbot.commands.CommandException;
 import me.tropicalfan344.musicbot.commands.MusicCommand;
 import me.tropicalfan344.musicbot.engines.impl.youtube.YouTubeTrack;
 import me.tropicalfan344.musicbot.utils.SimpleEmbedGenerator;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
@@ -21,7 +24,18 @@ public class CommandRickroll extends MusicCommand {
         event.deferReply().queue();
         GuildMusicManager manager = GuildMusicManager.getMusicManager(musicBot, event.getGuild());
         manager.add(YouTubeTrack.getVideoById("dQw4w9WgXcQ"));
-        manager.joinVoiceChannel(event.getMember().getVoiceState().getChannel());
+        if (!event.getGuild().getMemberById(event.getJDA().getSelfUser().getId()).getVoiceState().inAudioChannel()) {
+            GuildVoiceState selfVoiceState = event.getGuild().getMemberById(event.getJDA().getSelfUser().getId()).getVoiceState();
+            if (!event.getMember().getVoiceState().inAudioChannel()){
+                throw new CommandException("You are not in a voice channel right now!");
+            }
+            if (selfVoiceState.inAudioChannel()) {
+                throw new CommandException("The bot is already in a voice channel! Please use /leave before letting it join another.");
+            }
+
+            VoiceChannel voiceChannel = ((VoiceChannel) event.getMember().getVoiceState().getChannel());
+            GuildMusicManager.getMusicManager(musicBot, event.getGuild()).joinVoiceChannel(voiceChannel);
+        }
         event.getHook().editOriginalEmbeds(new EmbedBuilder()
                         .setColor(new Color(0xff4747))
                         .setImage("https://tenor.com/view/rick-roll-gif-23595798")
