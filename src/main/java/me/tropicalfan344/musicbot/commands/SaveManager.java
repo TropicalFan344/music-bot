46672a738438d75820c5cbba4f536eb9623604ac
tropicalfan344 <1080101@gm.fdhs.tyc.edu.tw>
2022-09-03 16:39:40 +0800

    fewf

--- src/main/java/me/tropicalfan344/musicbot/commands/SaveManager.java
+++ src/main/java/me/tropicalfan344/musicbot/commands/SaveManager.java
@@ -1,9 +1,37 @@
 package me.tropicalfan344.musicbot.commands;
 
 import com.google.gson.JsonObject;
+import me.tropicalfan344.musicbot.engines.Track;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class SaveManager {
 
-    public JsonObject load(String guildId)
+    final String guildId;
+    public SaveManager(String guildId) throws IOException {
+        this.guildId = guildId;
+        File saves = new File("saves");
+        File lists = new File(guildId + ".json");
+        if(!saves.exists())saves.mkdir();
+        if(!lists.exists())lists.createNewFile();
+    }
+
+    public void save(Track... tracks) {
+
+    }
+
+    public List<String> listSaves() {
+
+    }
+
+    public List<Track> load() {
+
+    }
+
+    public void delete() {
+
+    }
 
 }
